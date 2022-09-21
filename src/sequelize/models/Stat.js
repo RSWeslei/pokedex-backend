5f37f7ac0e1671b880a52fe8c01802a69deba7f5
RSWeslei <weslei67rosiak.com@gmail.com>
2022-09-20 22:11:59 -0300

    fix stats lenght

--- src/sequelize/models/Stat.js
+++ src/sequelize/models/Stat.js
@@ -10,58 +10,58 @@ const Stat = sequelize.define(
             autoIncrement: true
         },
         hp: {
-            type: DataTypes.NUMERIC(10, 4),
+            type: DataTypes.NUMERIC(10, 0),
             allowNull: false
         },
         attack: {
-            type: DataTypes.NUMERIC(10, 4),
+            type: DataTypes.NUMERIC(10, 0),
             allowNull: false
         },
         defense: {
-            type: DataTypes.NUMERIC(10, 4),
+            type: DataTypes.NUMERIC(10, 0),
             allowNull: false
         },
         speed: {
-            type: DataTypes.NUMERIC(10, 4),
+            type: DataTypes.NUMERIC(10, 0),
             allowNull: false
         },
         specialAttack: {
-            type: DataTypes.NUMERIC(10, 4),
+            type: DataTypes.NUMERIC(10, 0),
             allowNull: false,
             defaultValue: 0
         },
         specialDefense: {
-            type: DataTypes.NUMERIC(10, 4),
+            type: DataTypes.NUMERIC(10, 0),
             allowNull: false,
             defaultValue: 0
         },
         minHp: {
-            type: DataTypes.NUMERIC(10, 4),
+            type: DataTypes.NUMERIC(10, 0),
             allowNull: false,
             defaultValue: 0
         },
         minAttack: {
-            type: DataTypes.NUMERIC(10, 4),
+            type: DataTypes.NUMERIC(10, 0),
             allowNull: false,
             defaultValue: 0
         },
         minDefense: {
-            type: DataTypes.NUMERIC(10, 4),
+            type: DataTypes.NUMERIC(10, 0),
             allowNull: false,
             defaultValue: 0
         },
         minSpeed: {
-            type: DataTypes.NUMERIC(10, 4),
+            type: DataTypes.NUMERIC(10, 0),
             allowNull: false,
             defaultValue: 0
         },
         minSpecialAttack: {
-            type: DataTypes.NUMERIC(10, 4),
+            type: DataTypes.NUMERIC(10, 0),
             allowNull: false,
             defaultValue: 0
         },
         minSpecialDefense: {
-            type: DataTypes.NUMERIC(10, 4),
+            type: DataTypes.NUMERIC(10, 0),
             allowNull: false,
             defaultValue: 0
         },
