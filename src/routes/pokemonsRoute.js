f59f6d5877f4d9d6b5ca3fa448503d9833b3f9e7
RSWeslei <weslei67rosiak.com@gmail.com>
2022-10-12 16:15:18 -0300

    fix pokemon route

--- src/routes/pokemonsRoute.js
+++ src/routes/pokemonsRoute.js
@@ -1,8 +1,7 @@
 import controller from '../controllers/pokemonsController'
 
 export default (app) => {
-    app.get('/pokemons', controller.get)
-    app.get('/pokemons-home', controller.getAll)
+    app.get('/pokemons', controller.getAll)
     app.get('/pokemons/:id', controller.getById)
 }
 
